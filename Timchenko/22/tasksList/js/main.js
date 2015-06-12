265d2c5e70f5345d727ae5d5b8843e09563c7ccd
bogdan <bogdan@kredito.de>
2015-06-12 16:04:40 +0300

    22 list to do

--- Timchenko/22/tasksList/js/main.js
+++ Timchenko/22/tasksList/js/main.js
@@ -1,86 +1,102 @@
 $(document).ready(function () {
 
+	var Module = (function () {
 
-var Module = (function () {
+		/*var _elements = function (param) {
+			var 
+		};*/
 
-	/*var _elements = function (param) {
-		var 
-	};*/
+		var publicMethodOne = function (param) {
+			var holder = $('.' + param.holderClass);
+			var form = $('#' + param.formId);
+			var textarea = form.find('#' + param.textareaId);
+			var btnForm = form.find(param.btnForm);
+			var listItems = $('#' + param.listItemsId);
+			var btnEdit;
+			var btnDisable;
+			var editableElement;
+			
+			btnForm.on('click', function (e) {
+				e.preventDefault();
 
-	var publicMethodOne = function (param) {
-		var holder = $('.' + param.holderClass);
-		var form = $('#' + param.formId);
-		var textarea = form.find('#' + param.textareaId);
-		var btnForm = form.find(param.btnForm);
-		var listItems = $('#' + param.listItemsId);
-		var newItem = $(param.newItemd);
-		var btnEdit = $('<button>', {
-			class: param.btnEditClass,
-			text: param.btnEditText
-		});
-		var btnDisable = $('<button>', {
-			class: param.btnDisableClass,
-			text:param.btnDisableText
-		});
-		var editableElement = $('<span>', {
-			class: param.editableElement
-		});
+				newItem = $(param.newItemd);
 
-		btnForm.on('click', function (e) {
-			e.preventDefault();
-			listItems.append(newItem);
-			newItem.append(editableElement, btnEdit, btnDisable);
+				btnEdit = $('<button>', {
+					class: param.btnEditClass,
+					text: param.btnEditText
+				});
 
-			if (textarea.val().length > 0 ) {
-				editableElement.text(textarea.val());
-			} else {
-				return false;
-			}
-		});
+				btnDisable = $('<button>', {
+					class: param.btnDisableClass,
+					text:param.btnDisableText
+				});
 
-		btnEdit.on('click', function (e) {
-			e.preventDefault();
-			$(this).parent().addClass('active-edit');
-			$(this).siblings('span.edit-text').attr('contenteditable', 'true');
-		});
+				editableElement = $('<span>', {
+					class: param.editableElement
+				});
 
-		btnDisable.on('click', function (e) {
-			e.preventDefault();
-			$(this).siblings('span.edit-text')
-			.attr('contenteditable', 'false')
-			.parents('li')
-			.append('<em>')
+				if (textarea.val().length > 0 ) {
+					editableElement.text(textarea.val());
+					textarea.val('')
+				} else {
+					return false;
+				}
 
-		})
+				if (!listItems.children('li').length) {
+					newItem.append(editableElement, btnEdit, btnDisable);
+					listItems.append(newItem);
+				} else {
+					newItem.append(editableElement, btnEdit, btnDisable);
+					listItems.prepend(newItem);
+				};
 
+				btnEdit.on('click', function (e) {
+					e.preventDefault();
+					var currentList = $(this).parent('li');
+					console.log(this)
 
+					if(currentList.hasClass('active-edit')) {
+						currentList.removeClass('active-edit');
+						$(this).html('Start edit');
+						$(this).siblings('span.edit-text').attr('contenteditable', 'false');
+					} else {
+						currentList.addClass('active-edit');
+						$(this).html('Finish edit');
+						$(this).siblings('span.edit-text').attr('contenteditable', 'true');
+					}
+				});
 
+				btnDisable.on('click', function (e) {
+					e.preventDefault();
+					$(this).siblings('span.edit-text')
+					.attr('contenteditable', 'false')
+					.parents('li')
+					.append('<em>');
+					$(this).siblings('button.edit').hide();
+					$(this).html('completed')
+					
+				});
+			});
+		};
 
+		return {
+			publicMethodOne: publicMethodOne
+		};
 
+	})();
 
-
-
-	};
-
-
-	return {
-		publicMethodOne: publicMethodOne
-	};
-
-})();
-
-Module.publicMethodOne({
-	holderClass: 'wrapper',
-	formId: 'task-form',
-	textareaId: 'task-name',
-	btnForm: 'input[type="submit"]',
-	listItemsId: 'tasks-list',
-	newItemd: '<li>',
-	editableElement: 'edit-text',
-	btnEditClass: 'edit',
-	btnDisableClass: 'Disable',
-	btnEditText:'Edit',
-	btnDisableText: 'Disable'
-});
+	Module.publicMethodOne({
+		holderClass: 'wrapper',
+		formId: 'task-form',
+		textareaId: 'task-name',
+		btnForm: 'input[type="submit"]',
+		listItemsId: 'tasks-list',
+		newItemd: '<li>',
+		editableElement: 'edit-text',
+		btnEditClass: 'edit',
+		btnDisableClass: 'Disable',
+		btnEditText:'Start edit',
+		btnDisableText: 'Disable'
+	});
 
 });
